f7947170362afe0c88c37cebb4f31871b3ed6372
John Pertuz <johnpertuzsism@gmail.com>
2022-07-27 21:34:10 -0500

    adding functionality with javascript

---
+++ Js/index.js
@@ -0,0 +1,19 @@
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
+
+const TIE = 0;
+const WIN = 1;
+const LOST = 1;
+
+let isPlaying = false;
+
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+const resultText = document.getElementById("start-text");
+const userImg = document.getElementById("user-get");
+const machineImg = document.getElementById("computer-img");
+
+
+
